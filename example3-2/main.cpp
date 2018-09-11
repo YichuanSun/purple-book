75113a014356e181591fe1d213180be002b449e3
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-11 14:09:37 +0800

    好短的代码啊

--- example3-2/main.cpp
+++ example3-2/main.cpp
@@ -1,20 +1,40 @@
 #include <bits/stdc++.h>
-#define N 100005
 using namespace std;
-char a[N];
 char mp[]="`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
 int main()  {
-    gets(a);
-    int len=strlen(a);
-    for (int i=0;i<len;i++) {
-        if (a[i]==' ')  {
-            printf(" ");
-            continue;
-        }
-        char *ss=strchr(mp,a[i]);
-        int suffix=ss-mp;
-        printf("%c",mp[suffix-1]);
+    int i,c;
+    while ((c=getchar())!=EOF)  {
+        for (i=1;mp[i]&&mp[i]!=c;i++);
+        if (mp[i])  printf("%c",mp[i-1]);
+        else    printf("%c",c);
     }
-    printf("\n");
     return 0;
 }
+
+
+
+
+
+
+
+//自己写的，WA了，可能是因为输入里面有换行符
+//#include <bits/stdc++.h>
+//#define N 100005
+//using namespace std;
+//char a[N];
+//char mp[]="`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./";
+//int main()  {
+//    gets(a);
+//    int len=strlen(a);
+//    for (int i=0;i<len;i++) {
+//        if (a[i]==' ')  {
+//            printf(" ");
+//            continue;
+//        }
+//        char *ss=strchr(mp,a[i]);
+//        int suffix=ss-mp;
+//        printf("%c",mp[suffix-1]);
+//    }
+//    printf("\n");
+//    return 0;
+//}
