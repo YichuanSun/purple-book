5a3895724524fe4fe30da0e5c41ca824ea965adc
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-12 00:26:19 +0800

    坑死人的汉语叙述

--- example3-4/main.cpp
+++ example3-4/main.cpp
@@ -2,25 +2,37 @@
 #define N 1005
 using namespace std;
 int scf[N],ori[N];
-bool right[N],pres[N];
+bool rit[N],pres[N];
 int main()  {
     int n,times=1;
     while (scanf("%d",&n)&&n)   {
-        fill(right,right+n,false);
         fill(pres,pres+n,false);
-        for (int i=0;i<n;i++)   scanf("%d",ori+i);
+        for (int i=0;i<n;i++)   {
+            scanf("%d",ori+i);
+            pres[ori[i]]=true;
+        }
         printf("Game %d:\n",times++);
         while (true)    {
+            int cnt=0;
+            fill(rit,rit+20,false);
             int A=0,B=0;
             for (int i=0;i<n;i++)   {
                 scanf("%d",scf+i);
                 if (!scf[i])    cnt++;
-                //if (afind(ori,scf[i]))  pres[i]=true;
             }
             if (cnt==n) break;
+            for (int i=0;i<n;i++)   {
+                if (ori[i]==scf[i]) {
+                    A++;
+                    rit[scf[i]]=true;
+                }
+            }
             for (int i=0;i<n;i++)
-                if (ori[i]==scf[i]) right[ori[i]]=true;
-            printf("\t(%d,%d)\n",A,B);
+                if (!rit[scf[i]]&&pres[scf[i]]) {
+                    B++;
+                    rit[scf[i]]=true;
+                }
+            printf("    (%d,%d)\n",A,B);
         }
     }
     return 0;
