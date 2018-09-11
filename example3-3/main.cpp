d32b673d8593e6c2c3b04b0ef0be447a46c30f01
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-11 14:56:17 +0800

    想算法中

---
+++ example3-3/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+char *m="A   3  HIL JM O   2TUVWXY51SE Z  8 ";
+char *pf[]={"is not a palindrome.","is a regular palindrome.",
+"is a mirrored string."," is a mirrored palindrome."};
+char *s;
+int main()  {
+    int
+    while (scanf("%s",s)!=EOF)  {
+
+    }
+    return 0;
+}
