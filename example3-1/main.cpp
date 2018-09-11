927d432d3a6f52956496fcef1c56a69748ae9ae2
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-11 13:37:47 +0800

    看看汝佳大佬的程序，真是自惭形秽

--- example3-1/main.cpp
+++ example3-1/main.cpp
@@ -2,18 +2,33 @@
 using namespace std;
 
 int main()  {
-    string s;
-    bool p=true;
-    while (getline(cin,s))  {
-        for (unsigned i=0;i<s.size();i++) {
-            if (s[i]=='"'){
-                if (p)  cout<<"``";
-                else    cout<<"''";
-                p=!p;
-            }
-            else    cout<<s[i];
+    int c,q=1;
+    while ((c=getchar())!=EOF)    {
+        if (c=='"') {
+            printf("%s",q?"``":"''");
+            q=!q;
         }
-        cout<<endl;
+        else    printf("%c",c);
     }
     return 0;
 }
+
+//#include <bits/stdc++.h>
+//using namespace std;
+//
+//int main()  {
+//    string s;
+//    bool p=true;
+//    while (getline(cin,s))  {
+//        for (unsigned i=0;i<s.size();i++) {
+//            if (s[i]=='"'){
+//                if (p)  cout<<"``";
+//                else    cout<<"''";
+//                p=!p;
+//            }
+//            else    cout<<s[i];
+//        }
+//        cout<<endl;
+//    }
+//    return 0;
+//}
