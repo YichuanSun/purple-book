b958c15884b833faacfbcae7e4a7afd55e16be20
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-15 00:08:31 +0800

    换了个思路才搞成，没我想的那么简单，但是也没麻烦到哪里去

--- exercise3-10/main.cpp
+++ exercise3-10/main.cpp
@@ -1,31 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 typedef pair<int,int> pii;
 pii plain[10];
-bool spot[10];
+
+bool cmp(pii a,pii b) {
+    if (a.first==b.first)   return a.second<b.second;
+    return a.first<b.first;
+}
+bool eq(pii a,pii b)    {
+    if (a.first==b.first&&a.second==b.second)   return true;
+    else if (a.first==b.second&&a.second==b.first)  return true;
+    else return false;
+}
 int main()  {
+    //freopen("output.txt","wa",stdout);
     int cnt=0;
     pii tp;
     while (scanf("%d %d",&tp.first,&tp.second)!=EOF)    {
+        if (tp.first>tp.second) swap(tp.first,tp.second);
         plain[cnt%6]=tp;
         cnt++;
         if (cnt%6==0)   {
-            int combo=0;
-            for (int i=0;i<6;i++)   {
-                if (spot[i])    continue;
-                for (int j=i+1;j<6;j++) {
-                    if (spot[i]||spot[j])    continue;      //之前这里少了对spot的判断。因为接下来可能存在spot[i]
-                    //已被置为true的情况。比如说，三个面a，两个面b，一个面c，如果不加这一句，会输出possible，其实答案是impossible
-                    if ((plain[i].first==plain[j].first&&plain[i].second==plain[j].second)
-                        ||(plain[i].first==plain[j].second&&plain[i].second==plain[j].first))   {
-                            spot[i]=spot[j]=true;
-                            combo++;
-                    }
-                }
+            int cc=0;
+            bool flag=false;
+            pii lst;
+            sort(plain,plain+6,cmp);
+//            for (int i=0;i<6;i++)
+//                printf("%d: %d %d\n",i,plain[i].first,plain[i].second);
+            for (int i=0;i<6;i+=2)
+                if (eq(plain[i],plain[i+1]))    cc++;
+            if (cc==3)  {
+                int a=plain[0].first,b=plain[2].first;
+                int c=plain[0].second,d=plain[2].second;
+                if (a==b)   lst=make_pair(c,d);
+                else if (a==d)  lst=make_pair(b,c);
+                if (eq(lst,plain[4]))   flag=true;
             }
-            if (combo==3)   printf("POSSIBLE\n");
+            if (flag)   printf("POSSIBLE\n");
             else    printf("IMPOSSIBLE\n");
-            fill(spot,spot+10,false);
             fill(plain,plain+10,make_pair(0,0));
         }
     }
