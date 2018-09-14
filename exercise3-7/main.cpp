ac447d11eac6330cd0eb41ca91076efd5ddad06c
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-14 09:43:33 +0800

    容器题，二维向量映射数组

--- exercise3-7/main.cpp
+++ exercise3-7/main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 typedef map<char,int> mp;
 typedef pair<char,int> pp;
 vector<mp> v;
+vector<string> st;
 int main()  {
     ios::sync_with_stdio(false);
     cin.tie(0);
@@ -21,16 +22,25 @@ int main()  {
         }
         for (int i=0;i<m;i++)   {
             cin>>s;
+            st.push_back(s);
             for (int j=0;j<n;j++)   {
                 v[j][s[j]]++;
             }
         }
+//        for (int i=0;i<n;i++)   {
+//            mp::iterator it2=v[i].begin();
+//            cout<<i<<endl;
+//            for (;it2!=v[i].end();it2++)
+//                cout<<(*it2).first<<' '<<(*it2).second<<' ';
+//            cout<<endl;
+//        }
         string ans="";
+        //变量定义域没搞明白弄出的bug，两个这种bug
+        int mx=-1;
         for (int i=0;i<n;i++)   {
             char c;
-            int mx=-1;
             mp::iterator it1=v[i].begin();
-            for (;it1!=v[i].end();i++)    {
+            for (;it1!=v[i].end();it1++)    {
                 if ((*it1).second>mx)    {
                     mx=(*it1).second;
                     c=(*it1).first;
@@ -39,8 +49,16 @@ int main()  {
                     c=min(c,(*it1).first);
             }
             ans+=c;
+            mx=-1;
         }
-        cout<<ans<<endl;
+        int res=0;
+        for (int i=0;i<(int)st.size();i++) {
+            for (int j=0;j<(int)st[i].size();j++)
+                if (st[i][j]!=ans[j])   res++;
+        }
+        cout<<ans<<'\n'<<res<<endl;
+        v.clear();
+        st.clear();
     }
     return 0;
 }
