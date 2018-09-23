d6780ec3883f81af29befe5c11b200dbeeafd5ed
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-23 22:46:05 +0800

    .

--- exercise4-3/main.cpp
+++ exercise4-3/main.cpp
@@ -5,6 +5,7 @@ char cb[N][N];
 void cbl(char nw,char nx);
 void cbm(char nw,char nx);
 void cbq();
+bool check(int i,int j);
 vector<pair<int,int> > pp;
 int main()  {
     ios::sync_with_stdio(false);
@@ -36,31 +37,7 @@ int main()  {
 void cbl(char nw,char nx)   {
     for (int i=1;i<=8;i++)  {
         for (int j=1;j<=8;j++)  {
-            if (cb[i][j]!='-')  continue;
-            int it=i-1,jt;
-            while (it>=1&&cb[it][j]==nx)    it--;
-            if (it!=i-1&&it>=1&&cb[it][j]==nw)   {pp.push_back(make_pair(i,j));continue;}
-            it=i+1;
-            while (it<=8&&cb[it][j]==nx)    it++;
-            if (it!=i+1&&it<=8&&cb[it][j]==nw)   {pp.push_back(make_pair(i,j));continue;}
-            jt=j-1;
-            while (jt>=1&&cb[i][jt]==nx)    jt--;
-            if (jt!=j-1&&jt>=1&&cb[i][jt]==nw)   {pp.push_back(make_pair(i,j));continue;}
-            jt=j+1;
-            while (jt<=8&&cb[i][jt]==nx)    jt++;
-            if (jt!=j+1&&jt<=8&&cb[i][jt]==nw)   {pp.push_back(make_pair(i,j));continue;}
-            it=i-1,jt=j-1;
-            while (it>=1&&jt>=1&&cb[it][jt]==nx)    it--,jt--;
-            if (it!=i-1&&jt!=j-1&&it>=1&&jt>=1&&cb[it][jt]==nw)   {pp.push_back(make_pair(i,j));continue;}
-            it=i-1,jt=j+1;
-            while (it>=1&&jt<=8&&cb[it][jt]==nx)    it--,jt++;
-            if (it!=i-1&&jt!=j+1&&it>=1&&jt<=8&&cb[it][jt]==nw)   {pp.push_back(make_pair(i,j));continue;}
-            it=i+1,jt=j-1;
-            while (it<=8&&jt>=1&&cb[it][jt]==nx)    it++,jt--;
-            if (it!=i+1&&jt!=j-1&&it<=8&&jt>=1&&cb[it][jt]==nw)   {pp.push_back(make_pair(i,j));continue;}
-            it=i+1,jt=j+1;
-            while (it<=8&&jt<=8&&cb[it][jt]==nx)    it++,jt++;
-            if (it!=i+1&&jt!=j+1&&it<=8&&jt<=8&&cb[it][jt]==nw)   {pp.push_back(make_pair(i,j));continue;}
+            check(i,j);
         }
     }
     if (pp.empty()) {cout<<"No legal move."<<endl;return;}
@@ -70,13 +47,86 @@ void cbl(char nw,char nx)   {
     pp.clear();
 }
 
-void cbm(char nw,char nx)   {
+void cbm(char& w,char& x)   {
+    cin>>num;
+    if (!check(w,x))  {
+        cbm(x,w);
+    }
+    else {
 
+        swap(w,x);
+    }
 }
 void cbq()  {
 
 }
 
-
-
-
+bool check(int i,int j)
+{
+    if (cb[i][j]!='-')
+        return 0;
+    int it=i-1,jt;
+    while (it>=1&&cb[it][j]==nx)
+        it--;
+    if (it!=i-1&&it>=1&&cb[it][j]==nw)
+    {
+        pp.push_back(make_pair(i,j));
+        return 1;
+    }
+    it=i+1;
+    while (it<=8&&cb[it][j]==nx)
+        it++;
+    if (it!=i+1&&it<=8&&cb[it][j]==nw)
+    {
+        pp.push_back(make_pair(i,j));
+        return 1;
+    }
+    jt=j-1;
+    while (jt>=1&&cb[i][jt]==nx)
+        jt--;
+    if (jt!=j-1&&jt>=1&&cb[i][jt]==nw)
+    {
+        pp.push_back(make_pair(i,j));
+        return 1;
+    }
+    jt=j+1;
+    while (jt<=8&&cb[i][jt]==nx)
+        jt++;
+    if (jt!=j+1&&jt<=8&&cb[i][jt]==nw)
+    {
+        pp.push_back(make_pair(i,j));
+        return 1;
+    }
+    it=i-1,jt=j-1;
+    while (it>=1&&jt>=1&&cb[it][jt]==nx)
+        it--,jt--;
+    if (it!=i-1&&jt!=j-1&&it>=1&&jt>=1&&cb[it][jt]==nw)
+    {
+        pp.push_back(make_pair(i,j));
+        return 1;
+    }
+    it=i-1,jt=j+1;
+    while (it>=1&&jt<=8&&cb[it][jt]==nx)
+        it--,jt++;
+    if (it!=i-1&&jt!=j+1&&it>=1&&jt<=8&&cb[it][jt]==nw)
+    {
+        pp.push_back(make_pair(i,j));
+        return 1;
+    }
+    it=i+1,jt=j-1;
+    while (it<=8&&jt>=1&&cb[it][jt]==nx)
+        it++,jt--;
+    if (it!=i+1&&jt!=j-1&&it<=8&&jt>=1&&cb[it][jt]==nw)
+    {
+        pp.push_back(make_pair(i,j));
+        return 1;
+    }
+    it=i+1,jt=j+1;
+    while (it<=8&&jt<=8&&cb[it][jt]==nx)
+        it++,jt++;
+    if (it!=i+1&&jt!=j+1&&it<=8&&jt<=8&&cb[it][jt]==nw)
+    {
+        pp.push_back(make_pair(i,j));
+        return 1;
+    }
+}
