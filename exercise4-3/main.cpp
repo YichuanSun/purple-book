fce2cf98f461e6a974e0341086ae6ea315238c1b
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-24 00:00:51 +0800

    看着这坨狗屎一样的东西就发愁，大模拟

--- exercise4-3/main.cpp
+++ exercise4-3/main.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 char cb[N][N];
 void cbl(char nw,char nx);
-void cbm(char nw,char nx);
+void cbm();
 void cbq();
 bool check(int i,int j);
 vector<pair<int,int> > pp;
+char nw,nx;
+string num;
 int main()  {
     ios::sync_with_stdio(false);
     cin.tie(0);
@@ -16,7 +18,7 @@ int main()  {
         for (int i=1;i<=8;i++)
             for (int j=1;j<=8;j++)
                cin>>cb[i][j];
-        char ch='v',nw,nx;
+        char ch='v';
         cin>>nw;
         if (nw=='W')    nx='B';
         else if (nw=='B')   nx='W';
@@ -25,7 +27,7 @@ int main()  {
         while (ch!='Q') {
             switch(ch)  {
                 case 'L':   cbl(nw,nx);break;
-                case 'M':   cbm(nw,nx);break;
+                case 'M':   cbm();break;
             }
             cin>>ch;
         }
@@ -47,18 +49,59 @@ void cbl(char nw,char nx)   {
     pp.clear();
 }
 
-void cbm(char& w,char& x)   {
+void cbm()   {
     cin>>num;
-    if (!check(w,x))  {
-        cbm(x,w);
-    }
-    else {
-
-        swap(w,x);
-    }
+    int i=num[0],j=num[1];
+    if (!check(i,j))  swap(nx,nw);
+    pp.clear();
+    int it=i-1,jt;
+    while (it>=1&&cb[it][j]==nx)    it--;
+    if (it!=i-1&&it>=1&&cb[it][j]==nw)
+        for (int k=it+1;k<=i;k++)   cb[k][j]=nw;
+    it=i+1;
+    while (it<=8&&cb[it][j]==nx)    it++;
+    if (it!=i+1&&it<=8&&cb[it][j]==nw)
+        for (int k=i+1;k<=it;k++)   cb[k][j]=nw;
+    jt=j-1;
+    while (jt>=1&&cb[i][jt]==nx)    jt--;
+    if (jt!=j-1&&jt>=1&&cb[i][jt]==nw)
+        for (int k=jt+1;k<=j;k++)   cb[i][k]=nw;
+    jt=j+1;
+    while (jt<=8&&cb[i][jt]==nx)    jt++;
+    if (jt!=j+1&&jt<=8&&cb[i][jt]==nw)
+        for (int k=j+1;k<=jt;k++)   cb[i][k]=nw;
+    it=i-1,jt=j-1;
+    while (it>=1&&jt>=1&&cb[it][jt]==nx)    it--,jt--;
+    if (it!=i-1&&jt!=j-1&&it>=1&&jt>=1&&cb[it][jt]==nw)
+        for (int ip=it,jp=jt;ip<=i&&jp<=j;ip++,jp++)    cb[ip][jp]=nw;
+    it=i-1,jt=j+1;
+    while (it>=1&&jt<=8&&cb[it][jt]==nx)    it--,jt++;
+    if (it!=i-1&&jt!=j+1&&it>=1&&jt<=8&&cb[it][jt]==nw)
+        for (int ip=it,jp=jt;ip<=i&&jp>=j;ip++,jp--)    cb[ip][jp]=nw;
+    it=i+1,jt=j-1;
+    while (it<=8&&jt>=1&&cb[it][jt]==nx)    it++,jt--;
+    if (it!=i+1&&jt!=j-1&&it<=8&&jt>=1&&cb[it][jt]==nw)
+        for (int ip=it,jp=jt;ip>=i&&jp<=j;ip--,jp++)    cb[ip][jp]=nw;
+    it=i+1,jt=j+1;
+    while (it<=8&&jt<=8&&cb[it][jt]==nx)    it++,jt++;
+    if (it!=i+1&&jt!=j+1&&it<=8&&jt<=8&&cb[it][jt]==nw)
+        for (int ip=it,jp=jt;ip>=i&&jp>=j;ip--,jp--)    cb[ip][jp]=nw;
+    int black=0,white=0;
+    for (int i=1;i<=8;i++)
+        for (int j=1;j<=8;j++)  {
+            if (cb[i][j]=='B')  black++;
+            else if (cb[i][j]=='W') white++;
+        }
+    cout<<"Black - "<<black<<" White - "<<white<<endl;
+    swap(nx,nw);
 }
-void cbq()  {
 
+void cbq()  {
+    for (int i=1;i<=8;i++)  {
+        for (int j=1;j<=8;j++)
+            cout<<cb[i][j];
+        cout<<endl;
+    }
 }
 
 bool check(int i,int j)
@@ -129,4 +172,5 @@ bool check(int i,int j)
         pp.push_back(make_pair(i,j));
         return 1;
     }
+    return 0;
 }
