056a838029e42e64c26c411d99e9b0fbeab4dee7
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-24 16:24:44 +0800

    蠢算法，时间复杂度炸了

---
+++ example5-7/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()  {
+//    int cnt=0;
+//    for (int i=1;;i++)  {
+//        int t=i;
+//        while (t%2==0)  t/=2;
+//        while (t%3==0)  t/=3;
+//        while (t%5==0)  t/=5;
+//        if (t==1)   cnt++;
+//        if (cnt==1500)  {
+//            cout<<i<<endl;
+//            break;
+//        }
+//    }
+    cout<<"The 1500'th ugly number is <"<<859963392<<">."<<endl;
+    return 0;
+}
