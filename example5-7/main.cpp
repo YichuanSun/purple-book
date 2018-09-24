456977dc24d15cdd58e6835b1dcafabf4abc5e80
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-24 17:16:21 +0800

    ？

--- example5-7/main.cpp
+++ example5-7/main.cpp
@@ -1,19 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+int ge[4]={2,3,5};
+priority_queue<ll,vector<ll>,greater<ll> > pq;
+set<ll> s;
 int main()  {
-//    int cnt=0;
-//    for (int i=1;;i++)  {
-//        int t=i;
-//        while (t%2==0)  t/=2;
-//        while (t%3==0)  t/=3;
-//        while (t%5==0)  t/=5;
-//        if (t==1)   cnt++;
-//        if (cnt==1500)  {
-//            cout<<i<<endl;
-//            break;
-//        }
-//    }
-    cout<<"The 1500'th ugly number is <"<<859963392<<">."<<endl;
+    pq.push(1);
+    for (int i=1;;i++)  {
+        ll x=pq.top();pq.pop();
+        if (i==1500)    {
+            printf("The 1500'th ugly number is %lld.\n",x);
+            break;
+        }
+        for (int j=0;j<3;j++)   {
+            ll t=x*ge[j];
+            if (!s.count(t))    {s.insert(t),pq.push(t);}
+        }
+    }
     return 0;
 }
