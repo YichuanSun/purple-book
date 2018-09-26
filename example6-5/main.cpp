6078533526a6c12f2328bd120b59179c82e06044
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-26 12:41:48 +0800

    大致算是看明白了

--- example6-5/main.cpp
+++ example6-5/main.cpp
@@ -4,22 +4,35 @@ struct node{
     node *pre,*nex;
     int val;
 };
+node* listReverse(node* head);
 int main()  {
-    node* head=new node;
-    int n;
+    node* head=new node,*tail;
+    head->pre=NULL;
+    tail=head;
+    int n,t;
     cin>>n;
     for (int i=0;i<n;i++)   {
         cin>>t;
         node* tn=new node;
         tn->pre=head;
+        tn->nex=nullptr;
+        tn->val=t;
+        tail->nex=tn;
+        tail=tn;
     }
+    node* tt=listReverse(head);
+    while (tt)  {
+        cout<<tt->val<<' ';
+        tt=tt->nex;
+    }
+    cout<<endl;
     return 0;
 }
 
-listReverse(node* head)    {
+node* listReverse(node* head)    {
     node *pnode=head,*pre=NULL;
     while (pnode)   {
-        swap(pnode->next,pnode->pre);
+        swap(pnode->nex,pnode->pre);
         pre=pnode;
         pnode=pnode->pre;
     }
