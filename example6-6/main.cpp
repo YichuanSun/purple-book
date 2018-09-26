a84b133025e9d5ae3ee9e16bc639d999d8f69707
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-26 20:02:39 +0800

    excuse me

---
+++ example6-6/main.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool tree[1<<20+5];
+int dfs(int i);
+int n,d,l,a;
+int main()  {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin>>n;
+    while (n--) {
+        cin>>d>>l;
+        fill(tree,tree+(1<<d),false);
+        for (int i=1;i<=l;i++)
+            a=dfs(1);
+        cout<<a<<endl;
+    }
+    cin>>n;
+    return 0;
+}
+
+int dfs(int i) {
+    if (i>=(1<<(d-1)))  return i;
+    if (tree[i]==false) {
+        tree[i]=!tree[i];
+        return dfs(2*i);
+    }
+    else    {
+        tree[i]=!tree[i];
+        return dfs(2*i+1);
+    }
+}
