12444be47491a7b7f097f70e83e6f440cb771879
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-30 15:38:19 +0800

    忘了中序后序怎么建树了

--- example6-8/main.cpp
+++ example6-8/main.cpp
@@ -6,17 +6,25 @@ struct node{
     int val;
     node *left,*right;
 };
-int ino[N],post[N],ans=1000000,ta=0;
+int ans=1000000,ta=0,num;
+char ts[N];
 
 int read_arr(int* psp);
 node* buildTree(int* ino,int* post,int n);
 void dfs(node *root);
+void lfs(node* root);
 
 int main()  {
     int n=0;
-    while (read_arr(ino))   {
-        n=read_arr(post);
+    while (true)   {
+        //read_arr(ino)
+        //n=read_arr(post);
+        //printf("%d\n",n);
+        n=7;
+        int ino[N]={3,2,1,4,5,7,6};
+        int post[N]={3,1,2,5,6,7,4};
         node* root=buildTree(ino,post,n);
+        lfs(root);
         dfs(root);
         cout<<"dgsgsg"<<endl;
         printf("%d\n",ans);
@@ -24,14 +32,36 @@ int main()  {
     }
     return 0;
 }
+queue<node*> q;
+void lfs(node* root)    {
+    q.push(root);
+    while (!q.empty())  {
+        node* tq=q.front();
+        q.pop();
+        if (tq==NULL)   continue;
+        cout<<tq->val<<' ';
+        q.push(tq->left);
+        q.push(tq->right);
+    }
+    cout<<endl;
+}
 
 int read_arr(int* psp) {
-    int ee=0,c=0;
-    while ((c=getchar())&&c!='\n'){
-        if (c==EOF) return 0;
-        psp[ee++]=c-'0';
-    }
-    return ee;
+    return 1;
+    string line;
+    while (!getline(cin,line))  return 0;
+    stringstream ss(line);
+    int n=0,x;
+    while (ss>>x)   psp[n++]=x;
+    return n;
+//    int ee=0,c=0,i=0;
+//    gets(ts);
+//    while (i<(int)strlen(ts))    {
+//        sscanf(ts+i+1,"%d",&psp[ee++]);
+//        for (;i<(int)strlen(ts);i++)
+//            if (ts[i]==' ') break;
+//    }
+//    return ee;
 }
 
 node* buildTree(int* ino,int* post,int n)   {
@@ -47,11 +77,11 @@ node* buildTree(int* ino,int* post,int n)   {
 
 void dfs(node *root) {
     if (root==NULL) return;
-    cout<<root->val<<endl;
-    cout<<"-----"<<endl;
+    //cout<<root->val<<endl;
+    //cout<<"-----"<<endl;
     ta+=root->val;
     if (root->left==NULL&&root->right==NULL)    {
-        if (ans>ta) ans=ta;
+        if (ans>ta) {ans=ta,num=root->val;}
         return;
     }
     dfs(root->left);
