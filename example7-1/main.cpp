39e97c26cf35e4545e9cdb4beb49b1bff9ecbcf4
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-30 17:52:42 +0800

    PE？

---
+++ example7-1/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define N 11
+using namespace std;
+int si[N];
+int main()  {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n,cnt=0;
+    while (cin>>n&&n)   {
+        if (cnt++)  cout<<endl;
+        bool flag=false;
+        for (int i=1234;i<=98765;i++)    {
+            bool f=true;
+            int t=i;
+            if (t*n>99999||t*n<10000) continue;
+            int m=t*n;
+            while (m)   {
+                si[m%10]++;
+                m/=10;
+            }
+            if (i<10000) si[0]++;
+            while (t)   {
+                si[t%10]++;
+                t/=10;
+            }
+            for (int i=0;i<10;i++)
+                if (si[i]>1)    {f=false;break;}
+            if (f)  {printf("%d / %05d = %d\n",i*n,i,n);flag=true;}
+            fill(si,si+10,0);
+        }
+        if (!flag)  printf("There are no solutions for %d.\n",n);
+    }
+    return 0;
+}
