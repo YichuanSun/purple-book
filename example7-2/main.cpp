74592da7f04acba37426bb81c7d969568dea0796
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-09-30 19:37:09 +0800

    一道小题调了半小时，没想到最后是因为long long型的printf输出可能会出现问题，所以最后换cout就解决了

--- example7-2/main.cpp
+++ example7-2/main.cpp
@@ -1,22 +1,39 @@
-#include <bits/stdc++.h>
+#include <iostream>
 #define N 25
 using namespace std;
-int a[N],cnt,n;
-long long tms[N],ans;
 int main()  {
-    while (scanf("%d",&n)!=EOF)  {
-        if (cnt++)  printf("\n");
+    int n,cnt=0,a[N];
+    while (scanf("%d",&n)!=EOF&&n) {
+        long long ans=0;
         for (int i=0;i<n;i++)   scanf("%d",&a[i]);
-        tms[0]=1;
-        for (int i=1;i<=n;i++)   tms[i]=tms[i-1]*a[i-1];
-        printf("\n");
-        for (int i=0;i<=n;i++)   {
-            for (int j=i+1;j<=n;j++) {
-                long long t=tms[j]/tms[i];
+        for (int i=0;i<n;i++)   {
+            long long t=1;
+            for (int j=i;j<n;j++)   {
+                t*=a[j];
                 if (t>ans)  ans=t;
             }
         }
-        printf("Case #%d: The maximum product is %lld.\n",cnt,ans);
+        cout << "Case #" << ++cnt << ": The maximum product is " << ans << ".\n\n";
     }
     return 0;
 }
+
+//int a[N],cnt,n;
+//long long tms[N],ans;
+//int main()  {
+//    while (cin>>n)  {
+//        if (cnt++)  printf("\n");
+//        for (int i=0;i<n;i++)   cin>>a[i];
+//        tms[0]=1;
+//        for (int i=1;i<=n;i++)   tms[i]=tms[i-1]*a[i-1];
+//        cout<<endl;
+//        for (int i=0;i<=n;i++)   {
+//            for (int j=i+1;j<=n;j++) {
+//                long long t=tms[j]/tms[i];
+//                if (t>ans)  ans=t;
+//            }
+//        }
+//        cout<<"Case #"<<cnt<<": The maximum product is "<<ans<<".\n";
+//    }
+//    return 0;
+//}
