3369f2cce09244b887accbfdf7f36955031d2210
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-01 23:32:26 +0800

    呵

--- Eight Queens/main.cpp
+++ Eight Queens/main.cpp
@@ -1,14 +1,27 @@
 #include <bits/stdc++.h>
-#define N 16
+#define N 100
 using namespace std;
 int tot,n;
 int C[N];
+bool vis[3][N];
 void Search(int row);
 int main()  {
     cin>>n;
     Search(0);
     cout<<tot<<endl;
     return 0;
+}
+//vis数组，提高了搜索效率，确定三个方向上是否有其他皇后
+void Search(int row)    {
+    if (row>=n) tot++;
+    else for (int i=0;i<n;i++)  {
+        if (!vis[0][i]&&!vis[1][row+i]&&!vis[2][row-i+n])  {
+            C[row]=i;
+            vis[0][i]=vis[1][row+i]=vis[2][row-i+n]=1;
+            Search(row+1);
+            vis[0][i]=vis[1][row+i]=vis[2][row-i+n]=0;
+        }
+    }
 }
 
 //void Search(int row)    {
