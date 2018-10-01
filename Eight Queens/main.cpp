5c0adfc2ca1e270b4e4daaf0b51e81e1ff8b32f3
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-01 22:17:48 +0800

    嗯？

--- Eight Queens/main.cpp
+++ Eight Queens/main.cpp
@@ -11,15 +11,15 @@ int main()  {
     return 0;
 }
 
-void Search(int row)    {
-    if (row==n) tot++;
-    else
-        for (int i=0;i<n;i++)   {
-            C[row]=i;
-            int ok=1;
-            for (int j=0;j<row;j++)
-                if (C[row]==C[j]||row-C[row]==j-C[j]||row+C[row]==j+C[j])
-                    {ok=0;break;}
-            if (ok) Search(row+1);
-        }
-}
+//void Search(int row)    {
+//    if (row==n) tot++;
+//    else
+//        for (int i=0;i<n;i++)   {
+//            C[row]=i;
+//            int ok=1;
+//            for (int j=0;j<row;j++)
+//                if (C[row]==C[j]||row-C[row]==j-C[j]||row+C[row]==j+C[j])
+//                    {ok=0;break;}
+//            if (ok) Search(row+1);
+//        }
+//}
