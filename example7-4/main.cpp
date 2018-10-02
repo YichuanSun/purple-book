e8d8dbee28ff2d72c313de1e9357105b74bad0f6
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-02 23:16:44 +0800

    这套生成全排列的算法到12直接炸了

--- example7-4/main.cpp
+++ example7-4/main.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 #define N 20
 using namespace std;
-bool used[N];
+//bool used[N];
 int isp[N*N]={0,0,1},n;   //0为未处理，1为素数，2为合数
-vector<int> ans;
-void sech(int now,int rmn);
+//vector<int> ans;
+//void sech(int now,int rmn);
 void preope(){
     for (int i=1;i<N;i++)   {
         for (int j=1;j<N;j++)   {
@@ -18,38 +18,68 @@ void preope(){
     }
 }
 
+//int main()  {
+//    int cnt=0;
+//    preope();
+//    while (scanf("%d",&n)!=EOF){
+//        if (cnt++)  printf("\n");
+//        printf("Case %d:\n",cnt);
+//        ans.push_back(1);
+//        used[1]=true;
+//        sech(1,n-1);
+//        fill(used,used+n,false);
+//        ans.clear();
+//    }
+//    return 0;
+//}
+//
+//void sech(int now,int rmn)  {
+//    if (rmn==0) {
+//        if (isp[ans.back()+ans[0]]==1)  {
+//            printf("%d",ans[0]);
+//            for (int i=1;i<(int)ans.size();i++)
+//                printf(" %d",ans[i]);
+//            printf("\n");
+//        }
+//        return;
+//    }
+//    for (int i=2;i<=n;i++)  {
+//        if (!used[i]&&isp[i+now]==1) {
+//            ans.push_back(i);
+//            used[i]=true;
+//            sech(i,rmn-1);
+//            ans.pop_back();
+//            used[i]=false;
+//        }
+//    }
+//}
+int A[N];
+
 int main()  {
-    int cnt=0;
     preope();
-    while (scanf("%d",&n)!=EOF){
-        if (cnt++)  printf("\n");
-        printf("Case %d:\n",cnt);
-        ans.push_back(1);
-        used[1]=true;
-        sech(1,n-1);
-        fill(used,used+n,false);
-        ans.clear();
+    int n;
+    while (scanf("%d",&n)!=EOF) {
+        for (int i=0;i<n;i++)   A[i]=i+1;
+        do {
+            int e=1;
+            for (int i=1;i<n;i++)
+                if (isp[A[i]+A[i-1]]!=1)    {e=0;break;}
+            if (isp[A[0]+A[n-1]]!=1)    e=0;
+            if (e)  {
+                for (int i=0;i<n;i++)
+                    printf("%d ",A[i]);
+                printf("\n");
+            }
+        }while (next_permutation(A+1,A+n));
     }
     return 0;
 }
 
-void sech(int now,int rmn)  {
-    if (rmn==0) {
-        if (isp[ans.back()+ans[0]]==1)  {
-            printf("%d",ans[0]);
-            for (int i=1;i<(int)ans.size();i++)
-                printf(" %d",ans[i]);
-            printf("\n");
-        }
-        return;
-    }
-    for (int i=2;i<=n;i++)  {
-        if (!used[i]&&isp[i+now]==1) {
-            ans.push_back(i);
-            used[i]=true;
-            sech(i,rmn-1);
-            ans.pop_back();
-            used[i]=false;
-        }
-    }
-}
+
+
+
+
+
+
+
+
