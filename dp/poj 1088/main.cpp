9727c72019eb08df2283a1f1f4ce221178cf9960
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-09 21:03:34 +0800

    排序之后的DP，只是这样把数组打印出来之后我才发现，寻找路径是一件这么简单的事

--- dp/poj 1088/main.cpp
+++ dp/poj 1088/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <cstdio>
 #define N 105
 using namespace std;
-int r,c,h[N][N];
+int r,c,h[N][N],dp[N][N],ans;
 int dx[]={-1,1,0,0};
 int dy[]={0,0,-1,1};
 struct hill {
@@ -22,13 +23,25 @@ int main()  {
             scanf("%d",&h[i][j]);
             v.push_back(hill(h[i][j],i,j));
         }
+    fill(dp[0],dp[0]+N*N,1);
     sort(v.begin(),v.end(),cmp);
     for (int i=0;i<(int)v.size();i++)   {
         int nx=v[i].ii,ny=v[i].jj;
-
+        for (int s=0;s<4;s++)   {
+            int tx=nx+dx[s],ty=ny+dy[s];
+            if (tx<0||ty<0||tx>=r||ty>=c)   continue;
+            if (h[tx][ty]<h[nx][ny])
+                dp[nx][ny]=max(dp[nx][ny],dp[tx][ty]+1);
+        }
     }
+    for (int i=0;i<r;i++)   {
+        for (int j=0;j<c;j++){
+            //printf("%d ",dp[i][j]);
+            ans=max(ans,dp[i][j]);
+        }
+        //printf("\n");
+    }
+    printf("%d\n",ans);
     return 0;
 }
-
-
 
