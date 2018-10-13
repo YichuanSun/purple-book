7e0133abffdd6cd3d2cda88fe07c5ccd3675168f
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-13 23:49:06 +0800

    啊哈？

---
+++ dp/01package version 2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#define N 105
+#define M 10005
+using namespace std;
+int w[M],v[M],dp[N][M],n,W;
+int main()  {
+    int n;
+    cin>>n;
+    for (int i=0;i<n;i++)   cin>>w[i];
+    for (int i=0;i<n;i++)   cin>>v[i];
+    cin>>W;
+
+    return 0;
+}
