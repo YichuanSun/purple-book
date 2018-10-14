b2bc938615541cd20f8f523d9433f3998287ab41
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-14 10:24:38 +0800

    四个bug，详见下文
    第一个bug：没有考虑重复涂色情况，以为各个面的颜色是不同的。
    第二个bug：直接对未初始化的string使用了按下标赋值，就错了
    第三个bug：用erase套循环，删除对应下标的元素时，忘了erase会使下标改变，所以可能导致遍历不全。
    第四个bug：现在还没找到，应该是思路上的，导致了WA

--- exercise4-4/main.cpp
+++ exercise4-4/main.cpp
@@ -1,35 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string tot1(string ts);
-string tot2(string ts);
-string tot3(string ts);
+string tot1(string ts)  {swap(ts[1],ts[2]),swap(ts[2],ts[4]),swap(ts[3],ts[4]);return ts;}
+string tot2(string ts)  {swap(ts[1],ts[4]),swap(ts[2],ts[3]);return ts;}
+string tot3(string ts)  {swap(ts[1],ts[3]),swap(ts[2],ts[3]),swap(ts[3],ts[4]);return ts;}
 
 int main()  {
     ios::sync_with_stdio(false);
     cin.tie(0);
-    freopen("output.txt","wa",stdout);
+    //freopen("output.txt","wa",stdout);
     string s;
     while (cin>>s)  {
         string s1=s.substr(0,6);
         string s2=s.substr(6);
+        int fg=0;
         for (int i=0;i<6;i++)   {   //选定其中一个面为顶
+            //cout<<"Case: "<<i<<endl;
             if (s1[i]!=s2[0])   continue;
-            string ts,ts1=s1;
+            string ts,ts1;
+            for (int j=0;j<6;j++)   ts1+=s1[j];
+            //cout<<"ts1=\t"<<ts1<<endl;
             char cc=ts1[5-i];
-            ts[0]=ts1[i];
-            ts1.erase(5-i);
+            ts+=ts1[i];
+            //cout<<"ts[0]=\t"<<ts1[i]<<endl;
+            //cout<<"now ts=\t"<<ts<<endl;
+            ts1[5-i]='Z',ts1[i]='Z';        //这个直接循环删除erase不行啊，会改变元素次序，删除也就遍历不全了。
+            int k=0;
+            while (k<ts1.size())    {
+                if (ts1[k]=='Z')    ts1.erase(k,1);
+                else k++;
+            }
             ts+=ts1;
             ts+=cc;
-            if (s2==ts||s2==tot1(ts)||s2==tot2(ts)||s2==tot3(ts))
-                cout<<"TRUE"<<endl;
-            else    cout<<"FALSE"<<endl;
-            break;
+            //cout<<"cc=\t"<<cc<<endl;
+//            cout<<ts<<endl;
+//            cout<<tot1(ts)<<endl;
+//            cout<<tot2(ts)<<endl;
+//            cout<<tot3(ts)<<endl;
+            if (s2==ts||s2==tot1(ts)||s2==tot2(ts)||s2==tot3(ts))   {fg=1;break;}
         }
+        if (fg) cout<<"TRUE"<<endl;
+        else cout<<"FALSE"<<endl;
     }
     return 0;
 }
-
-string tot1(string ts)  {swap(ts[1],ts[2]),swap(ts[2],ts[4]),swap(ts[3],ts[4]);return ts;}
-string tot2(string ts)  {swap(ts[1],ts[4]),swap(ts[2],ts[3]);return ts;}
-string tot3(string ts)  {swap(ts[1],ts[3]),swap(ts[2],ts[3]),swap(ts[3],ts[4]);return ts;}
