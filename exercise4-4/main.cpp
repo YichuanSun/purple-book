a3b40b1b7a295b6294316e33a2e4cfb738171e11
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-14 10:32:53 +0800

    妈耶，终于找到bug了

--- exercise4-4/main.cpp
+++ exercise4-4/main.cpp
@@ -1,3 +1,7 @@
+//我知道问题出在哪里了，我应该先把我选定的面转到顶上去
+//再旋转周围的四个面，而不是像这样粗鲁的全部压到数组中去
+//也就是说，这个算法获得的顶面周围的四个面是错误的
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -17,14 +21,13 @@ int main()  {
         for (int i=0;i<6;i++)   {   //选定其中一个面为顶
             //cout<<"Case: "<<i<<endl;
             if (s1[i]!=s2[0])   continue;
-            string ts,ts1;
-            for (int j=0;j<6;j++)   ts1+=s1[j];
+            string ts,ts1=s1;
             //cout<<"ts1=\t"<<ts1<<endl;
             char cc=ts1[5-i];
             ts+=ts1[i];
             //cout<<"ts[0]=\t"<<ts1[i]<<endl;
             //cout<<"now ts=\t"<<ts<<endl;
-            ts1[5-i]='Z',ts1[i]='Z';        //这个直接循环删除erase不行啊，会改变元素次序，删除也就遍历不全了。
+            ts1[5-i]='Z',ts1[i]='Z';        //这个直接循环删除erase不行啊，会改变元素次序，也就遍历不全了。
             int k=0;
             while (k<ts1.size())    {
                 if (ts1[k]=='Z')    ts1.erase(k,1);
