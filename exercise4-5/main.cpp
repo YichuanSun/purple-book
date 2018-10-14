09c05c877f95e2c4b5a83bb95c818a8786816286
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-14 18:28:05 +0800

    我为什么这么菜？

--- exercise4-5/main.cpp
+++ exercise4-5/main.cpp
@@ -1,9 +1,15 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
+#define INF 10000005
 using namespace std;
+string ss;
+int main()  {
+    int n,a=INF,b,c,d;
+    cin>>n;
+    for (int i=0;i<n;i++)   {
+        cin>>ta>>tb>>tc>>td;
+        a=min(ta,a),b=min(tb,b),c=min(tc,c),d=min(td,d);
 
-int main()
-{
-    cout << "Hello world!" << endl;
+    }
+
     return 0;
 }
