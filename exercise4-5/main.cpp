7fb4cc8fddba55313d1b4b08e5f5dc2b8540b9c9
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-15 20:45:54 +0800

    water...

--- exercise4-5/main.cpp
+++ exercise4-5/main.cpp
@@ -1,15 +1,24 @@
 #include <bits/stdc++.h>
 #define INF 10000005
 using namespace std;
-string ss;
+
 int main()  {
     int n,a=INF,b,c,d;
     cin>>n;
     for (int i=0;i<n;i++)   {
         cin>>ta>>tb>>tc>>td;
         a=min(ta,a),b=min(tb,b),c=min(tc,c),d=min(td,d);
-
     }
-
+    string sa=ex(a),sb=ex(b),sc=ex(c);
     return 0;
+}
+
+string ex(int x)    {
+    string ts;
+    if (!x) {ts="0";return ts;}
+    while (x)   {
+        ts+=(x&1);
+        x>>=1;
+    }
+    return ts;
 }
