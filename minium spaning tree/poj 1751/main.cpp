840626cd4830e7a1bccfe2407f1f3ef68325e99b
Yichuan Sun <30886176+YichuanSun@users.noreply.github.com>
2018-10-16 20:21:12 +0800

    蛤？

--- minium spaning tree/poj 1751/main.cpp
+++ minium spaning tree/poj 1751/main.cpp
@@ -6,18 +6,17 @@
 #define N 1005
 using namespace std;
 
-vector<pair<int,int> > pii;
-struct edge{int v1,v2;double cost;};
-vector<edge> ve;
-int parent[N],n,m;
+vector<pair<int,int> > pii;             //存储每个点的x，y坐标，点集
+struct edge{int v1,v2;double cost;};    //定义边的数据结构
+vector<edge> ve;                        //边集
+int parent[N],n,m;                      //parent是并查集的存储数组，某个点i的父节点为parent[i]
 
-double len(int a,int b);
-bool cmp(edge a,edge b) {return a.cost<b.cost;}
-bool cmpp(pair<int,int> a,pair<int,int> b)  {return a.first<b.first;}
-void UFset();
-void Union(int a,int b);
-int Find(int a);
-bool same(int a,int b);
+double len(int a,int b);        //计算两点间距
+bool cmp(edge a,edge b) {return a.cost<b.cost;}     //对边排序的比较函数
+void UFset();                   //并查集的初始化，用数组存并查集，将每个点的父节点都设为-1.根节点的父节点值是个负数，绝对值表示当前集合的元素个数
+void Union(int a,int b);        //将两个点加入同一个集合
+int Find(int a);                //寻找某个点所在的集合（以这一集合的根节点表示），上三个并查集函数是并查集的核心
+bool same(int a,int b);         //判断两点是否在同一个集合内
 
 int main()  {
     pair<int,int> tpii;
@@ -32,31 +31,29 @@ int main()  {
     //cout<<"pii:\t"<<pii.size()<<endl;
     for (int i=0;i<(int)pii.size();i++) {
         for (int j=i;j<(int)pii.size();j++)   {
-            te.v1=i,te.v2=j;    //无向图，又忘了，边得输入两次
+            te.v1=i,te.v2=j;    //无向图，又忘了，边得输入两次，但是在这题不是邻接矩阵，而相当于邻接表，所以只输入一条联通边即可。
             if (i==j)   te.cost=0;
             else    te.cost=len(i,j);
             //cout<<te.cost<<endl;
             ve.push_back(te);
-            //swap(te.v1,te.v2);
-            //ve.push_back(te);
         }
     }
     cin>>m;
     for (int i=0;i<m;i++)   {
         int t1,t2;
         cin>>t1>>t2;
-        Union(t1-1,t2-1);
+        Union(t1-1,t2-1);       //将已经相连的点连接，因为我是从0开始的，但是输入从1开始，所以此处-1
     }
 //    for (int i=0;i<n;i++)
 //        cout<<parent[i]<<' ';
 //    cout<<endl;
-    sort(ve.begin(),ve.end(),cmp);
+    sort(ve.begin(),ve.end(),cmp);      //对边排序，kruskal算法的要求
     //cout<<"TABLE\n";
 //    for (int i=0;i<(int)ve.size();i++)
 //        cout<<ve[i].v1<<' '<<ve[i].v2<<' '<<ve[i].cost<<endl;
     //cout<<"ve:\t"<<ve.size()<<endl;
     for (int i=0;i<(int)ve.size();i++)  {
-        if (!same(ve[i].v1,ve[i].v2))   {
+        if (!same(ve[i].v1,ve[i].v2))   {       //如果没在同一个并查集中，说明这两点未相连，就连接他们两个
             Union(ve[i].v1,ve[i].v2);
             cout<<ve[i].v1+1<<' '<<ve[i].v2+1<<endl;
         }
